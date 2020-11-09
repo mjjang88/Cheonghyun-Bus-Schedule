6c7c89891ce808f71b412f237a852cdc9ae069fa
mjjang <inseok16@gmail.com>
2020-11-09 23:59:18 +0900

    release 1.0.0

--- app/src/main/java/com/mjjang/cheonghyunbusschedule/ui/MainActivity.kt
+++ app/src/main/java/com/mjjang/cheonghyunbusschedule/ui/MainActivity.kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil.setContentView
 import com.mjjang.cheonghyunbusschedule.R
 import com.mjjang.cheonghyunbusschedule.databinding.ActivityMainBinding
+import com.mjjang.cheonghyunbusschedule.util.VersionManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        VersionManager.doVersionCheck(this)
     }
 }
